8030944da569b29e6b8a00d91aa590d8cedbcec3
Andrew Shershunov <mail@evilur.ru>
2025-12-13 00:12:11 +0300

    docs: Add empty string test

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,10 @@
 #include "hash_map/hash_map.h"
 #include "linked_list/linked_list.h"
+#include "string/string.h"
 
 int main() {
     linked_list_unit_test();
     hash_map_unit_test();
+    string_unit_test();
     return 0;
 }
---
+++ test/string/string.cpp
@@ -0,0 +1,7 @@
+#include "string.h"
+#include "type/string.h"
+
+#include "../core/unit_test.h"
+
+void string_unit_test() {
+}
